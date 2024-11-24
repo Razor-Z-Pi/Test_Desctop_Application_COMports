5fb82223ba36eb8e2659d1f2c519091b9d0955ec
Razor-Z-Pi <popovp.popoff2001@gmail.com>
2024-11-24 21:57:35 +0700

    new program

---
+++ ComPortsDesctop/ControllerComPotrMain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComPortsDesctop
+{
+    static class ControllerComPotrMain
+    {
+        public static ControllerCOMPort cOMPort { get; set; }
+    }
+}
---
+++ ComPortsDesctop/ProgressDataController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComPortsDesctop
+{
+    public class ProgressDataController : INotifyPropertyChanged
+    {
+        private static ProgressDataController _instance;
+
+        public static ProgressDataController Instance => _instance ??= new ProgressDataController();
+
+        private string id;
+        public string SomeValue
+        {
+            get => id;
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged(nameof(SomeValue));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
